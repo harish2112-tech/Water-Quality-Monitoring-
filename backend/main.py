e6e961dd8662e6de8b21cc48aa3c8c063cea530b
harishgame21 <harishgame21@gmail.com>
2026-03-22 23:17:46 +0530

    register error correction

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+# Add the current directory to sys.path so 'app' module can be found when running on Vercel
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
