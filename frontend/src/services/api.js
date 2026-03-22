6c67704dc680d95b7fca9fef706fe19f9dcfddec
harishgame21 <harishgame21@gmail.com>
2026-03-22 23:30:49 +0530

    resgister 404 error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === "production" ? "/api" : "http://127.0.0.1:8000",
+  baseURL: process.env.NODE_ENV === "production" ? "" : "http://127.0.0.1:8000",
   headers: { "Content-Type": "application/json" },
 });
 
